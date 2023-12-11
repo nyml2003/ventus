1edc64710d0d40d0dbfcfcc31e4bcb9db8e6b841
nyml2003 <nyml2003>
2023-12-11 23:17:43 +0800

    打包

---
+++ gui/dist/spa/assets/QCardSection.7caf841b.js
@@ -0,0 +1,1 @@
+import{c as r,h as e}from"./render.d396e546.js";import{c as n,h as c}from"./index.c9b20d8c.js";var l=r({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:o}){const a=n(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>c(t.tag,{class:a.value},e(o.default))}});export{l as Q};
