61c70771723c8cd7b258b3ea02c5ed48cc1054ae
nyml2003 <nyml2003.com>
2024-01-10 19:14:11 +0800

    aa

--- ventus/urls.py
+++ ventus/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
-    path('game/', include('game.urls')),
+    path('api/blog/', include('blog.urls')),
+    path('api/game/', include('game.urls')),
 ]
